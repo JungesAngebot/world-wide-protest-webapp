de27476a8e1bb4e8f56c34e7b1ceaa6a371db07e
noel-byT <noel-lang@outlook.com>
2017-04-09 00:52:58 +0200

    Added Icon Font

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -4,22 +4,22 @@ import { Link } from 'react-router';
 const Footer = () => {
 
 	return (
-	<footer id="keel">
-      <div className="inner">
-        <div id="keel-nav">
-          <ul>
-            <li><a href="/imprint">Imprint</a></li>
-            <li><a href="/legal">Legal</a></li>
-          </ul>
-        </div>
-        <div id="social-nav">
-          <ul>
-            <li><a href="https://www.facebook.com/wwprotest" target="_blank" title="world wide protest on facebook"><span className="icon icon-facebook"></span></a></li>
-            <li><a href="https://twitter.com/wwprotest" target="_blank" title="world wide protest on twitter"><span className="icon icon-twitter"></span></a></li>
-          </ul>
-        </div>
-      </div>
-    </footer>
+		<footer id="keel">
+			<div className="inner">
+				<div id="keel-nav">
+					<ul>
+						<li><a href="/imprint">Imprint</a></li>
+						<li><a href="/legal">Legal</a></li>
+					</ul>
+				</div>
+				<div id="social-nav">
+					<ul>
+						<li><a href="https://www.facebook.com/wwprotest" target="_blank" title="world wide protest on facebook"><span className="icon icon-facebook"></span></a></li>
+						<li><a href="https://twitter.com/wwprotest" target="_blank" title="world wide protest on twitter"><span className="icon icon-twitter"></span></a></li>
+					</ul>
+				</div>
+			</div>
+		</footer>
 	);
 
 };
