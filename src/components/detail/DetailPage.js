ff0a96060668b255cb7411f338e25c663063f83c
noel-byT <noel-lang@outlook.com>
2017-04-09 02:43:39 +0200

    DetailPage Styling

--- src/components/detail/DetailPage.js
+++ src/components/detail/DetailPage.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import * as apiActions from 'actions/apiActions';
+import Footer from 'components/common/Footer';
 
 class HomePage extends Component {
 
@@ -18,12 +19,26 @@ class HomePage extends Component {
 	render() {
 		let event = this.props.api.currentEvent;
 		return (
-			<div>
+			<div class="details-page">
 				<div id="detail-header" style={{ backgroundImage: `url(${require("../../styles/img/event-placeholder.jpg")})` }}>
 					<div className="text-gradient">
 						<h1>{event.title}</h1>
 					</div>
 				</div>
+				<div className="grid-container meta-container">
+
+					<div className="col-2 meta-wrapper">
+						<div class="meta">
+							<p className="card-info"><i className="icon icon-date"></i> 09.04.2017, 2 pm - 3 pm</p>
+						</div>
+					</div>
+
+					<div className="col-4">
+						{event.description}
+					</div>
+
+				</div>
+				<Footer />
 			</div>
 		);
 
@@ -44,11 +59,11 @@ function mapDispatchToProps(dispatch) {
 }
 
 export function loadDetailPage(nextState, store, next) {
-	store.dispatch(apiActions.setPageLoad(false));
+	// store.dispatch(apiActions.setPageLoad(false));
 	store.dispatch(apiActions.getEventById(nextState.params.id))
 	.then(response => {
 		store.dispatch(apiActions.setEvent(response.data));
-		store.dispatch(apiActions.setPageLoad(true));
+		// store.dispatch(apiActions.setPageLoad(true));
 		next();
 	})
 }
